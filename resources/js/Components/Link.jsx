a68040c01d164b7f6584475768ef6217e0315a56
Eric Seyden <ericseyden@yahoo.com>
2024-11-18 14:02:23 -0700

    add some documentation to link component due to curiosity

--- resources/js/Components/Link.jsx
+++ resources/js/Components/Link.jsx
@@ -1,18 +1,22 @@
 /**
- * TODO: Update this component to use your client-side framework's link
+ * Link - Tailwind UI Catalyst Link Component
+ *
+ * Wraps links in DataInteractive to add data-focus and data-hover attributes.
+ *
+ * Maybe: Update this component to use your client-side framework's link
  * component. We've provided examples of how to do this for Next.js, Remix, and
  * Inertia.js in the Catalyst documentation:
  *
  * https://catalyst.tailwindui.com/docs#client-side-router-integration
  */
 
-import * as Headless from '@headlessui/react'
-import React, { forwardRef } from 'react'
+import * as Headless from "@headlessui/react";
+import React, { forwardRef } from "react";
 
 export const Link = forwardRef(function Link(props, ref) {
-  return (
-    <Headless.DataInteractive>
-      <a {...props} ref={ref} />
-    </Headless.DataInteractive>
-  )
-})
+    return (
+        <Headless.DataInteractive>
+            <a {...props} ref={ref} />
+        </Headless.DataInteractive>
+    );
+});
